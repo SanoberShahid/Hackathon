04b47d9530b781b561bb68cfaa5108a95d4e49f8
SanoberShahid <160754505+SanoberShahid@users.noreply.github.com>
2024-11-08 12:45:41 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+ 
+        document.getElementById('toggle-skills').addEventListener('click', function () {
+            const skillsSection = document.getElementById('skills');
+            if (skillsSection.style.display === 'none') {
+                skillsSection.style.display = 'block';
+            } else {
+                skillsSection.style.display = 'none';
+            }
+            
+        });
+        
+
